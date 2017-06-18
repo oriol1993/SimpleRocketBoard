47eb42a3098385f605e6c6f130630ce6252af588
oriol1993 <oriolcasamor@gmail.com>
2017-06-18 19:23:14 +0200

    Button Added

--- Main/Main.ino
+++ Main/Main.ino
@@ -1,9 +1,49 @@
+
+#define LED 3
+#define PB 5
+uint8_t state = 0;
+uint32_t last_bttn = 0;
+uint32_t led_flash = 0;
+uint8_t led_state = 0;
+
 void setup()
 {
-	1234
+	pinMode(LED, OUTPUT);
+	pinMode(PB, INPUT_PULLUP);
+	Serial.begin(9600);
 }
 
 void loop()
 {
-blablablabla
+	buttonCheck();
+	checkLed();
+}
+
+void buttonCheck()
+{
+	if (millis() - last_bttn > 300)
+	{
+		if (digitalRead(PB) == 0){ 
+			state = !state;
+			last_bttn = millis();
+			led_flash = millis();
+		}
+	}
+}
+
+void checkLed()
+{
+	if (state == true)
+	{
+		if (millis() - led_flash > 500)
+		{
+			led_state = !led_state;
+			led_flash = millis();
+			digitalWrite(LED, led_state);
+		}
+	}
+	else {
+		digitalWrite(LED, LOW);
+	}
+
 }
